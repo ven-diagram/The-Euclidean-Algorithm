84dde0bd4d42d865442f848cc773f0f39d861d20
Vismaya Venkateshwara <98354509+ven-diagram@users.noreply.github.com>
2022-07-29 19:20:30 +0100

    Update euclideanAlgorithm.js

--- euclideanAlgorithm.js
+++ euclideanAlgorithm.js
@@ -1,7 +1,13 @@
 function euclideanAlgorithm(x, y, a = x, b = y) {
+  
+  //If y is greater than x then switch the numbers around
   if (y>x)
     [x, y] = [y, x];
+  
+  // r is the remiander of y over x
   let r = x%y;
+  
+  //recursive approach - keep performing the algorithm until r is 0
   return r == 0
   ? "The gcd(" + a+", "+b+") is " + y
   : euclideanAlgorithm(y, r, a, b);
